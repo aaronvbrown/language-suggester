3dd360422f1b3b467d147b25e6b6a1f5657fc27f
Aaron Brown <worldofaaron@hotmail.com>
2023-06-17 17:20:23 -0700

    begin adding js

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,13 @@
 
 // User Interface Logic
 
+window.onload = function() {
+    let form = document.querySelector("form");
+    form.onsubmit = function(event) {
+        const question1Answer = document.getElementById("question1Input");
+        const question2Answer = document.getElementById("question2Input");
+        const question3Answer = document.getElementById("question3Input");
+        const question4Answer = document.getElementById("question4Input");
+        const question5Answer = document.getElementById("question5Input");
+    }
+}
