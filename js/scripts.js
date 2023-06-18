1f8add4065082a752ca7cf4a200de8d415bfe2ba
Aaron Brown <worldofaaron@hotmail.com>
2023-06-18 08:14:14 -0700

    correct errors in event listener

--- js/scripts.js
+++ js/scripts.js
@@ -13,12 +13,17 @@ window.addEventListener("load", function() {
       const question3Input = document.getElementById("question3Input").value;
       const question4Input = document.getElementById("question4Input").value;
       const question5Input = document.getElementById("question5Input").value;
+      if (document.getElementById("question1Input") != null) {
+        console.log(question1Input)
+        document.getElementById("python").removeAttribute("class");
+      }
       event.preventDefault();
     });
   
     form.addEventListener("submit", function() {
       suggestion.removeAttribute("class");
       event.preventDefault();
+      
     }); 
     
   
